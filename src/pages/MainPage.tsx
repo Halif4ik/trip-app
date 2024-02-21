c9b4b17d7f693c1fe84cf5fafb266a878e075589
DimaGrimach <chiki.briki2@gmail.com>
2024-02-21 18:11:02 +0200

    working forecast

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,16 +1,64 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {TSearchParams, useSearchCityQuery} from "../store/forecast/forecast.api";
 
 export function MainPage() {
    const searchObj: TSearchParams = {
       searchCity: 'Odessa',
       startDate: '2024-02-22',
-      endDate: '2024-02-23'
-
+      endDate: '2024-02-28'
    }
-   const {isLoading, isError, data} = useSearchCityQuery(searchObj)
+   const [searchForecast, setStateForecast] = useState('')
+   const [dropdown, setDropdown] = useState(false)
+   const [debounced, setDebounced] = useState('');
+
+   const {isLoading, isError, data} = useSearchCityQuery({
+          ...searchObj,
+          searchCity: debounced,
+       },
+       {
+          skip: debounced.length < 4,
+          refetchOnFocus: true
+       });
+
+   useEffect(() => {
+      const handler: NodeJS.Timeout = setTimeout(() => setDebounced(searchForecast), 1300);
+      return () => clearTimeout(handler);
+   }, [searchForecast]);
+
+   useEffect(() => {
+      console.log('useEffect-', debounced);
+      setDropdown(debounced.length > 4 && !!data && data.length > 0);
+   }, [debounced, data]);
 
    return (
-       <div>Home</div>
+       <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
+          {isError && <p className="text-center text-red-600">Something went wrong...</p>}
+
+          <div className="relative w-[660px]">
+             <input
+                 type="text"
+                 className="border py-2 px-4 w-full h-[42px] mb-2"
+                 placeholder="Search for city in weather.visualcrossing..."
+                 value={searchForecast}
+                 onChange={e => setStateForecast(e.target.value)}
+             />
+             {dropdown &&
+                 <ul className="list-none absolute top-[42px] left-0 right-0 max-h-[200px] overflow-y-scroll shadow-md bg-white">
+                    {isLoading && <p className="text-center">Loading...</p>}
+                    {data?.map(oneDay => (
+                        <li key={oneDay.datetime}
+                            className="py-2 px-4 hover:bg-gray-400 hover:text-white transition-colors cursor-pointer"
+                        >temp-:{oneDay.temp}</li>
+                    ))}
+                 </ul>}
+
+
+             {/*  <div className="container">
+                { areReposLoading && <p className="text-center">Repos are loading...</p> }
+                { repos?.map(repo => <RepoCard repo={repo} key={repo.id} />) }
+             </div>*/}
+          </div>
+
+       </div>
    )
 }
--- src/store/forecast/forecast.api.ts
+++ src/store/forecast/forecast.api.ts
@@ -1,6 +1,8 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import {IDay, IForecastResponse} from "../interfaces";
+import {BaseQueryMeta, BaseQueryResult} from "@reduxjs/toolkit/dist/query/baseQueryTypes";
 
-const baseUrl = process.env.REACT_APP_BASE_URL;
+const baseUrl = process.env.BASE_URL;
 export type TSearchParams = {
    searchCity: string
    startDate: string
@@ -13,16 +15,20 @@ export const forecastApi = createApi({
    }),
    refetchOnFocus: true,
    endpoints: build => ({
-      searchCity: build.query<any, TSearchParams>({
+      searchCity: build.query<IDay[], TSearchParams>({
          query: (searchParams: TSearchParams) => {
             console.log('baseUrl-', baseUrl);
             return {
-               url: `${searchParams.searchCity},UA/${searchParams.startDate}/${searchParams.endDate}`,
+               url: `${searchParams.searchCity}/${searchParams.startDate}/${searchParams.endDate}`,
                params: {
+                  unitGroup: 'metric',
+                  include: 'days',
                   key: '2DLEMQ8VMWQZ4BRRGN83WXMG7',
+                  contentType: 'json'
                }
             }
          },
+         transformResponse:(response:IForecastResponse) =>response.days
       }),
 
    })
